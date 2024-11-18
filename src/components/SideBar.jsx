620b619e08e7838bd50bbf1652a1cdb892c86418
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-11-18 15:54:10 +0330

    logo

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../ui/Logo";
+
+function SideBar() {
+  return (
+    <div className=" h-[90vh]">
+      <Logo />
+    </div>
+  );
+}
+
+export default SideBar;
