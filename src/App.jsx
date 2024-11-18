d597271437d4ec36f3b0fb08d219179f8c67a996
danial alijanianzadeh <88320221+danialajz82@users.noreply.github.com>
2024-11-18 11:43:39 +0330

    default app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,7 @@
 import React from "react";
 
 function App() {
-  return (
-    // <div className="container">
-    <div className="text-center text-2xl text-blue-500">سسس</div>
-    // <div className="main"></div>
-    // </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
